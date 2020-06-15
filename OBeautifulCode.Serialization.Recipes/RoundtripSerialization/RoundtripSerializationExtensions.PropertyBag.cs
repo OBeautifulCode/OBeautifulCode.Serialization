fadca011528123e0e47de01e1385cf8db385bb0a
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-06-15 12:56:05 -0400

    add more useful overloads to roundtrip serialization extensions

--- OBeautifulCode.Serialization.Recipes/RoundtripSerialization/RoundtripSerializationExtensions.PropertyBag.cs
+++ OBeautifulCode.Serialization.Recipes/RoundtripSerialization/RoundtripSerializationExtensions.PropertyBag.cs
@@ -52,6 +52,28 @@ namespace OBeautifulCode.Serialization.Recipes
         /// <param name="appDomainScenarios">Optional value that specifies various scenarios of serializing and de-serializing in the current App Domain or a new App Domain.  DEFAULT is test the roundtrip in a new App Domain and also to serialize in a new App Domain and de-serialize in a new, but different App Domain.</param>
         public static void RoundtripSerializeViaPropertyBagWithBeEqualToAssertion<T>(
             this T expected,
+            PropertyBagSerializationConfigurationType propertyBagSerializationConfigurationType = null,
+            IReadOnlyCollection<SerializationFormat> formats = null,
+            AppDomainScenarios appDomainScenarios = DefaultAppDomainScenarios)
+        {
+            expected.RoundtripSerializeViaPropertyBagWithBeEqualToAssertion(
+                propertyBagSerializationConfigurationType.ConcreteSerializationConfigurationDerivativeType,
+                formats,
+                appDomainScenarios);
+        }
+
+        /// <summary>
+        /// Test roundtrip serialization to/from a Property Bag, asserting that the expected/provided value is equal to the deserialized value using
+        /// <see cref="Verifications.BeEqualTo{T}(AssertionTracker, T, string, ApplyBecause, System.Collections.IDictionary)"/>,
+        /// with the serialization configuration type specified.
+        /// </summary>
+        /// <typeparam name="T">The type being tested.</typeparam>
+        /// <param name="expected">The value to serialize, which should be equal to the resulting deserialized object.</param>
+        /// <param name="propertyBagSerializationConfigurationType">Optional type of the serialization configuration to use for Property Bag testing.  DEFAULT is null; <see cref="NullPropertyBagSerializationConfiguration"/> will be used.</param>
+        /// <param name="formats">The serialization formats to test.</param>
+        /// <param name="appDomainScenarios">Optional value that specifies various scenarios of serializing and de-serializing in the current App Domain or a new App Domain.  DEFAULT is test the roundtrip in a new App Domain and also to serialize in a new App Domain and de-serialize in a new, but different App Domain.</param>
+        public static void RoundtripSerializeViaPropertyBagWithBeEqualToAssertion<T>(
+            this T expected,
             Type propertyBagSerializationConfigurationType = null,
             IReadOnlyCollection<SerializationFormat> formats = null,
             AppDomainScenarios appDomainScenarios = DefaultAppDomainScenarios)
@@ -63,6 +85,30 @@ namespace OBeautifulCode.Serialization.Recipes
                 false,
                 false,
                 true,
+                formats,
+                appDomainScenarios);
+        }
+
+        /// <summary>
+        /// Test roundtrip serialization to/from a Property Bag, asserting that the expected/provided value is equal to the deserialized value using
+        /// the specified callback, with the serialization configuration type specified.
+        /// </summary>
+        /// <typeparam name="T">The type being tested.</typeparam>
+        /// <param name="expected">The value to serialize, which should be equal to the resulting deserialized object.</param>
+        /// <param name="verificationCallback">Callback to verify that the expected/provided value is equal to the deserialized value.</param>
+        /// <param name="propertyBagSerializationConfigurationType">Optional type of the serialization configuration to use for Property Bag testing.  DEFAULT is null; <see cref="NullPropertyBagSerializationConfiguration"/> will be used.</param>
+        /// <param name="formats">The serialization formats to test.</param>
+        /// <param name="appDomainScenarios">Optional value that specifies various scenarios of serializing and de-serializing in the current App Domain or a new App Domain.  DEFAULT is test the roundtrip in a new App Domain and also to serialize in a new App Domain and de-serialize in a new, but different App Domain.</param>
+        public static void RoundtripSerializeViaPropertyBagWithCallbackVerification<T>(
+            this T expected,
+            RoundtripSerializationVerification<T> verificationCallback,
+            PropertyBagSerializationConfigurationType propertyBagSerializationConfigurationType = null,
+            IReadOnlyCollection<SerializationFormat> formats = null,
+            AppDomainScenarios appDomainScenarios = DefaultAppDomainScenarios)
+        {
+            expected.RoundtripSerializeViaPropertyBagWithCallbackVerification(
+                verificationCallback,
+                propertyBagSerializationConfigurationType.ConcreteSerializationConfigurationDerivativeType,
                 formats,
                 appDomainScenarios);
         }
