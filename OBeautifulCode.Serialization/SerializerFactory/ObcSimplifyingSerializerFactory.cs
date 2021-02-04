db797b9fd9c15cc760bf88616f407a7614f9726a
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-02-04 11:52:31 -0500

    add ObcSimplifyingSerializerFactory

---
+++ OBeautifulCode.Serialization/SerializerFactory/ObcSimplifyingSerializerFactory.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObcSimplifyingSerializerFactory.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Serialization
+{
+    using OBeautifulCode.Representation.System;
+
+    /// <summary>
+    /// A serializer factory that wraps the serializers built by a backing factory with an <see cref="ObcSimplifyingSerializer"/>.
+    /// </summary>
+    public class ObcSimplifyingSerializerFactory : ISerializerFactory
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObcSimplifyingSerializerFactory"/> class.
+        /// </summary>
+        /// <param name="backingSerializerFactory">The backing serializer factory.</param>
+        protected ObcSimplifyingSerializerFactory(
+            ISerializerFactory backingSerializerFactory)
+        {
+            this.BackingSerializerFactory = backingSerializerFactory;
+        }
+
+        /// <summary>
+        /// Gets the backing serializer factory.
+        /// </summary>
+        public ISerializerFactory BackingSerializerFactory { get; }
+
+        /// <inheritdoc />
+        public ISerializer BuildSerializer(
+            SerializerRepresentation serializerRepresentation,
+            AssemblyMatchStrategy assemblyMatchStrategy = AssemblyMatchStrategy.AnySingleVersion)
+        {
+            var fallbackSerializer = this.BackingSerializerFactory.BuildSerializer(serializerRepresentation, assemblyMatchStrategy);
+
+            var result = new ObcSimplifyingSerializer(fallbackSerializer);
+
+            return result;
+        }
+    }
+}
