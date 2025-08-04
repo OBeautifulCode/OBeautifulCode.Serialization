67f3a6058f026cbfa75f2f2c372a7a46044335c2
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-08-04 14:36:04 -0400

    Added ObcNullSerializer

---
+++ OBeautifulCode.Serialization/ObcSerializer/ObcNullSerializer.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObcNullSerializer.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Serialization
+{
+    using System;
+
+    /// <summary>
+    /// Null object pattern implementation of an <see cref="ISerializer"/>.
+    /// </summary>
+    public class ObcNullSerializer : ISerializer
+    {
+        private static readonly SerializerRepresentation CachedSerializerRepresentation = new SerializerRepresentation(SerializationKind.Proprietary);
+
+        /// <inheritdoc />
+        public SerializationConfigurationType SerializationConfigurationType => null;
+
+        /// <inheritdoc />
+        public SerializationKind SerializationKind => CachedSerializerRepresentation.SerializationKind;
+
+        /// <inheritdoc />
+        public SerializerRepresentation SerializerRepresentation => CachedSerializerRepresentation;
+
+        /// <inheritdoc />
+        public byte[] SerializeToBytes(
+            object objectToSerialize) => null;
+
+        /// <inheritdoc />
+        public string SerializeToString(
+            object objectToSerialize) => null;
+
+        /// <inheritdoc />
+        public T Deserialize<T>(
+            string serializedString) => default;
+
+        /// <inheritdoc />
+        public object Deserialize(
+            string serializedString,
+            Type type) => null;
+
+        /// <inheritdoc />
+        public T Deserialize<T>(
+            byte[] serializedBytes) => default;
+
+        /// <inheritdoc />
+        public object Deserialize(
+            byte[] serializedBytes,
+            Type type) => null;
+    }
+}
